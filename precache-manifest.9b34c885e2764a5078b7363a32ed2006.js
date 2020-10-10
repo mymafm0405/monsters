807873fa60ede89c525815cc17e70075a9f28f76
Mahmoud <mymafm0405@gmail.com>
2020-10-10 19:07:44 +0300

    Updates

---
+++ precache-manifest.9b34c885e2764a5078b7363a32ed2006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fcf6b0ea9283a5982761f7e826f616a",
+    "url": "/mymafm0405/monsters.git/index.html"
+  },
+  {
+    "revision": "7a710c3d699165d4a2ee",
+    "url": "/mymafm0405/monsters.git/static/css/main.15f7cecc.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/mymafm0405/monsters.git/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mymafm0405/monsters.git/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a710c3d699165d4a2ee",
+    "url": "/mymafm0405/monsters.git/static/js/main.d8c2c4d5.chunk.js"
+  },
+  {
+    "revision": "0c5a1504fe893e6fb913",
+    "url": "/mymafm0405/monsters.git/static/js/runtime-main.7ad0456c.js"
+  }
+]);
